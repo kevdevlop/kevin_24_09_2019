2f86280a68f61fa055199119ce764bac95623f3e
kevin lopez <kevin@sfleet.mx>
2019-09-24 19:35:32 -0500

    Ajax get Data

--- src/AjaxRequest.js
+++ src/AjaxRequest.js
@@ -2,15 +2,31 @@ import React, {Component} from 'react'
 
 class AjaxRequest extends Component{
     state={
-        data: undefined
+        data: ""
     }
     componentDidMount(){
         this.get_request()
     }
     get_request(){
-        fetch("https://www.reddit.com/r/all.json").then((result) => {
-            this.setState({data: "Success response"})
-        }).catch(error => {
+        fetch("/data.json")
+        .then(res => {
+            res.json().then(result => {
+                let data = result.data.map(element => {
+                    return (
+                        <div>
+                            <h3>Item no. {element.id}</h3>
+                            <h5>Address</h5>
+                            <p>Street: {element.info.address.street}</p>
+                            <p>Number: {element.info.address.number}</p>
+                            <hr/>
+                        </div>
+                    )
+                });
+                this.setState({data})
+            })
+            
+        })
+        .catch(error => {
             this.setState({data: "Error in request"})
         })
     }
